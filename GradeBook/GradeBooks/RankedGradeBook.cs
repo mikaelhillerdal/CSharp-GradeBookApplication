bc51482e68f8989d4091948e7b3ac1ad9b541ee1
Mikael Hillerdal <mikael.hillerdal@gmail.com>
2018-12-12 10:51:29 +0100

    done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -44,7 +44,8 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             base.CalculateStatistics();
         }
@@ -53,7 +54,8 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Need more than 5 Students");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
             base.CalculateStudentStatistics(name);
         }
