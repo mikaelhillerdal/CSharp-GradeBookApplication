5e062983a0c940f75f03ef9894f1c72368280b10
Mikael Hillerdal <mikael.hillerdal@gmail.com>
2018-12-13 10:31:32 +0100

    module 7 test 1

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,12 +40,13 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-
+            bool isWeighted = Convert.ToBoolean(parts[3]);
+                        
             BaseGradeBook gradeBook;
             if (type == "standard")
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             else if (type == "ranked")
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
